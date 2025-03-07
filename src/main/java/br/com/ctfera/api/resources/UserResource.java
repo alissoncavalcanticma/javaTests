d39afaf553e31e19b3ea5db061d2df02b6018ccc
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-03-07 00:10:19 -0300

    implemented UserResource and create properties for local environment

--- src/main/java/br/com/ctfera/api/resources/UserResource.java
+++ src/main/java/br/com/ctfera/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.ctfera.api.resources;
 
 import br.com.ctfera.api.domain.User;
+import br.com.ctfera.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1,"Alisson", "alisson@alisson.com", "123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
--- src/main/java/br/com/ctfera/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/ctfera/api/services/impl/UserServiceImpl.java
@@ -1,17 +1,24 @@
 package br.com.ctfera.api.services.impl;
 
 import br.com.ctfera.api.domain.User;
+import br.com.ctfera.api.repository.UserRepository;
 import br.com.ctfera.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User findById(Integer Id) {
-        return null;
+        Optional<User> obj = userRepository.findById(Id);
+        return obj.orElse(null);
     }
 
     @Override
