c3aae28fb91e265ac31bac6466bfe3dc6184fe40
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-03-06 21:39:41 -0300

    initial commit

---
+++ src/test/java/br/com/ctfera/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ctfera.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
