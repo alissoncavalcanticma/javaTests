26761381157a1de4d7a2c10f104672b221275ca2
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-03-06 23:26:25 -0300

    Created UserRepository

---
+++ src/main/java/br/com/ctfera/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.ctfera.api.repository;
+
+import br.com.ctfera.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
